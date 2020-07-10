0be634ec024a9110864ee68da8c0c10cc5ace4dd
Tapas Meher <tapasmeher@gmail.com>
2020-07-10 23:57:27 +0530

    file read write basic
    file read write basic

---
+++ small_project/gym_diet_management_system.py
@@ -0,0 +1,30 @@
+"""
+Date:10-07-2020
+Description : Data retrive or data log ?
+1. Create for three client(Select Client)
+
+	Harry,Rohan,Hammad
+2. What Do you want to log?
+	6 Files, 2 for each
+	1. Exercise done in the day
+	2. Food had each day
+
+with time stamp
+
+10-Jul-20 : 1 Roti, 1 Dal
+20-Jul-20 :	1 Roti, 1 checken
+---------------------------------------------------------------------
+# Health Management System
+# 3 clients - Harry, Rohan and Hammad
+
+def getdate():
+    import datetime
+    return datetime.datetime.now()
+
+# Total 6 files
+# write a function that when executed takes as input client name
+# One more function to retrieve exercise or food for any client
+------------------------------------------------------
+"""
+
+fp = open()
